1b9d2b1604007752657658969916a283f4c36084
SenaT03 <81272285+SenaT03@users.noreply.github.com>
2023-05-09 15:41:41 +1000

    Update LinkedList.cpp
    - added deconstructer
    - added size()
    - added base code for removeItem

--- A2/startup_code/LinkedList.cpp
+++ A2/startup_code/LinkedList.cpp
@@ -1,25 +1,48 @@
 #include "LinkedList.h"
-
 #include "Node.h"
 #include <iostream>
-#include <string>
 #include "helper.h"
-
 using std::string;
 using std::vector;
 
-LinkedList::LinkedList() {
-   head = nullptr;
-   count = 0;
+#include <string>
 
+// constructer, initilialzing head pointer to null and count to 0
+LinkedList::LinkedList()
+{
+    this->head = nullptr;
+    this->count = 0;
 }
 
-bool LinkedList::insert(std::vector<std::string> data){
+// Deconstructer, freeing up memory
+LinkedList::~LinkedList()
+{
+    Node *head = this->head;
+    while (head != nullptr)
+    {
+        if (head != nullptr)
+        {
+            Node *toDelete = head;
+            head = head->next;
+
+            delete toDelete;
+            toDelete = nullptr;
+        }
+        else
+        {
+            throw std::runtime_error("Nothing to remove");
+        }
+    }
+}
+
+bool LinkedList::insert(std::vector<std::string> data)
+{
 
     std::vector<std::string> price_tokens = {};
     Helper::splitString(data[3], price_tokens, ".");
 
-    if (price_tokens.size() != 2){
+    if (price_tokens.size() != 2)
+    {
         std::cout << "Price Error" << std::endl;
         return false;
     }
@@ -27,7 +50,6 @@ bool LinkedList::insert(std::vector<std::string> data){
     Price price = *(new Price());
     price.dollars = std::stoi(price_tokens[0]);
     price.cents = std::stoi(price_tokens[1]);
-
 
     Stock *new_stock = new Stock();
     new_stock->id = data[0];
@@ -39,47 +61,91 @@ bool LinkedList::insert(std::vector<std::string> data){
     Node *new_node = new Node();
     new_node->data = new_stock;
 
- 
-    Node* current = head;
-    Node* prev = nullptr;
+    Node *current = head;
+    Node *prev = nullptr;
 
-    while (current != nullptr  && current->data->name < new_stock->name) {
+    while (current != nullptr && current->data->name < new_stock->name)
+    {
         prev = current;
         current = current->next;
     }
 
-    if (prev == nullptr){
+    if (prev == nullptr)
+    {
         new_node->next = head;
         head = new_node;
-    }else{
+    }
+    else
+    {
         prev->next = new_node;
         new_node->next = current;
     }
-
 
     count += 1;
     return true;
 }
 
-string LinkedList::toString(){
+string LinkedList::toString()
+{
     string new_string = "";
-    Node* current = head;
+    Node *current = head;
 
-    while (current) {
+    while (current)
+    {
         Stock *data = current->data;
-        new_string = new_string+ "\n"+
-        data->id
-        +"|"+data->name
-        +"\t\t\t\t"
-        +"|"+std::to_string(data->on_hand)
-        +"$ "+std::to_string(data->price.dollars) +"."+std::to_string(data->price.cents);
-        
+        new_string = new_string + "\n" +
+                     data->id + "|" + data->name + "\t\t\t\t" + "|" + std::to_string(data->on_hand) + "$ " + std::to_string(data->price.dollars) + "." + std::to_string(data->price.cents);
 
         current = current->next;
     }
     return new_string;
 }
 
-LinkedList::~LinkedList() {
-    // TODO
+// void LinkedList::remove_index(int index)
+// {
+//     Node *prev = nullptr, *current;
+
+//     if (index >= 0 && index < size())
+//     {
+//         if (head != nullptr)
+//         {
+//             int counter = 0;
+//             Node *current = head;
+//             // pre should point to node before current;
+//             Node *prev = nullptr;
+
+//             while (counter != index)
+//             {
+//                 ++counter;
+//                 prev = current;
+//                 current = current->next;
+//             }
+
+//             if (prev == nullptr)
+//             {
+//                 head = current->next;
+//             }
+//             else
+//             {
+//                 prev->next = current->next;
+//             }
+
+//             //delete current->card;
+//             delete current;
+//         }
+//     }
+// }
+
+int LinkedList::size()
+{
+
+    int length = 0;
+
+    Node *current = head;
+    while (current != nullptr)
+    {
+        ++length;
+        current = current->next;
+    }
+    return length;
 }
